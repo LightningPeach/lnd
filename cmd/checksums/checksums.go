2531549a03bb8c84cf76a1aebe26aa05a3d8b43b
evgeniy.scherbina <evgeniy.scherbina@bitfury.com>
2018-11-05 17:59:14 +0200

    fixed generate.go

--- cmd/checksums/checksums.go
+++ cmd/checksums/checksums.go
@@ -18,9 +18,9 @@ const (
 )
 
 const (
-	blockHeadersCheckSum     = "f37fda67ab7a9d4697c8a0b36151fb84222418415bf41c0153ee7cf81716fd50"
-	regFilterHeadersCheckSum = "c3220ffc5096c040faa3e23661fcb2d0fe6509dd74a300f99b9d5973b88348dc"
-	neutrinoDBCheckSum       = "d3c1f7fd57a18120c24baf672aad549a9ea1ef9e623eb16f5d8886f2aaf030d4"
+	defaultBlockHeadersCheckSum     = "9cd44bce499a85763a273c62ba8787fa298edd31d377dd14fa387d79bb6ca8ea"
+	defaultRegFilterHeadersCheckSum = "b993ae7440443b7caaf19ecd5269b97180066c3fca393e9556b4bf74642a7cd8"
+	defaultNeutrinoDBCheckSum       = "985db27d6ead3849d061bbe82512a2aacd2101d87509fcf1d4705e3ff8e7106c"
 )
 
 func checkErr(err error) {
@@ -34,22 +34,48 @@ func main() {
 	dirHelp := fmt.Sprintf("path to directory with presynced files(%v%v%v)",
 		blockHeadersFilename, regFilterHeadersFilename, neutrinoDBFilename)
 	dir := flag.String("dir", "neutrino", dirHelp)
+	show := flag.Bool("show", false, "generate and show checksums")
+
+	blockHeadersCheckSum := flag.String("block_headers_check_sum", defaultBlockHeadersCheckSum, "")
+	regFilterHeadersCheckSum := flag.String("reg_filter_headers_check_sum", defaultRegFilterHeadersCheckSum, "")
+	neutrinoDBCheckSum := flag.String("neutrino_db_check_sum", defaultNeutrinoDBCheckSum, "")
+
 	flag.Parse()
 
 	blockHeadersRaw, err := ioutil.ReadFile(filepath.Join(*dir, blockHeadersFilename))
 	checkErr(err)
 	checkSum := sha256.Sum256(blockHeadersRaw)
-	assertEq(hex.EncodeToString(checkSum[:]), blockHeadersCheckSum, "bad block headers's checksum")
+	checkSumHex := hex.EncodeToString(checkSum[:])
+	if *show {
+		fmt.Println(checkSumHex)
+	} else {
+		assertEq(checkSumHex, *blockHeadersCheckSum,
+			fmt.Sprintf("bad block headers's checksum; actual: %v, want: %v", checkSumHex, blockHeadersCheckSum))
+	}
+
 
 	regFilterHeadersRaw, err := ioutil.ReadFile(filepath.Join(*dir, regFilterHeadersFilename))
 	checkErr(err)
 	checkSum = sha256.Sum256(regFilterHeadersRaw)
-	assertEq(hex.EncodeToString(checkSum[:]), regFilterHeadersCheckSum, "bad reg filter headers's checksum")
+	checkSumHex = hex.EncodeToString(checkSum[:])
+	if *show {
+		fmt.Println(checkSumHex)
+	} else {
+		assertEq(checkSumHex, *regFilterHeadersCheckSum,
+			fmt.Sprintf("bad reg filter headers's checksum; actual: %v, want: %v", checkSumHex, regFilterHeadersCheckSum))
+	}
 
 	neutrinoDBRaw, err := ioutil.ReadFile(filepath.Join(*dir, neutrinoDBFilename))
 	checkErr(err)
 	checkSum = sha256.Sum256(neutrinoDBRaw)
-	assertEq(hex.EncodeToString(checkSum[:]), neutrinoDBCheckSum, "bad neutrino db's checksum")
+	checkSumHex = hex.EncodeToString(checkSum[:])
+	if *show {
+		fmt.Println(checkSumHex)
+	} else {
+		assertEq(checkSumHex, *neutrinoDBCheckSum,
+			fmt.Sprintf("bad neutrino db's checksum; actual: %v, want: %v", checkSumHex, neutrinoDBCheckSum))
+	}
+	fmt.Println("OK")
 }
 
 func assertEq(left, right, meta string) {
